1c53f8074a683ab9461285ca63196a9a92c64bb3
hontas <lundin.pontus@gmail.com>
2021-06-06 23:58:20 +0200

    remove unused contexts

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ import { getScreensData } from './src/store/reducers/screens';
 import { getChapters } from './src/store/reducers/chapters';
 import { getConfig } from './src/store/reducers/config';
 import { getSavedGameState } from './src/store/reducers/game';
-import { UserProvider } from './src/context/userContext';
 import { RootNavigator } from './src/navigators/rootNavigator';
 
 const store = configureStore();
@@ -44,11 +43,9 @@ function App() {
   return (
     <Provider store={store}>
       <StatusBar barStyle="light-content" />
-      <UserProvider>
         <NavigationContainer>
           <RootNavigator />
         </NavigationContainer>
-      </UserProvider>
     </Provider>
   );
 }
