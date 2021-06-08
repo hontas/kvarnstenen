29caa0023a9814d920659fdfd3b80dcd3980c91e
hontas <lundin.pontus@gmail.com>
2021-06-09 01:59:17 +0200

    set version in app.config.js

--- app.config.js
+++ app.config.js
@@ -1,15 +1,20 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const version = 8;
+
 export default ({ config }) => {
   const combinedConfig = {
     ...config,
+    version: `0.0.${version}`,
     ios: {
       ...config.ios,
+      buildNumber: `0.0.${version}`,
       bundleIdentifier: process.env.APP_BUNDLE_ID,
     },
     android: {
       ...config.android,
+      versionCode: version,
       package: process.env.APP_BUNDLE_ID,
       config: {
         ...config.android.config,
